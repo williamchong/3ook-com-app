74c927590ec4530c7d1b7b4129368b14c6448ba4
Ng Wing Tat, David <i@ngwingt.at>
2026-01-20 10:37:15 +0800

    ✨ Add user agent & identifier to webview

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,8 @@ import { StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { WebView } from 'react-native-webview';
 
+import packageJson from '../package.json';
+
 export default function App() {
   const insets = useSafeAreaInsets();
   return (
@@ -9,9 +11,10 @@ export default function App() {
       <View style={{ ...styles.topSpacer, height: insets.top }} />
       <View style={{ ...styles.container, paddingBottom: insets.bottom }}>
         <WebView
-          source={{ uri: 'https://3ook.com' }}
+          source={{ uri: 'https://3ook.com?app=1' }}
           originWhitelist={['*']}
           style={styles.webview}
+          userAgent={`3ook-com-app/${packageJson.version}`}
         />
       </View>
     </>
